ffc6fc115e2cc123602e24f136ae80f871f4515e
L-Garay <garay.logan@gmail.com>
2022-03-24 13:23:10 -0600

    switch to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isLoggedIn ? (
+          <h1>Congratulations, you are logged in!</h1>
+        ) : (
+          <>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </>
+        )}
       </header>
     </div>
   );
