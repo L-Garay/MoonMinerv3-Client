0dafa04c50bbb5a7bb4c2e6fbd21a2b55af92688
L-Garay <garay.logan@gmail.com>
2022-03-24 13:25:55 -0600

    create react app with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Learn React
+              Learn React with TypeScript
             </a>
           </>
         )}
