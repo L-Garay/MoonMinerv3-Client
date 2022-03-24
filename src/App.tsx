2f692de895f4fe7f2387fd0109d7c62ef6deb07c
L-Garay <garay.logan@gmail.com>
2022-03-24 14:16:31 -0600

    auth0 initial configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,46 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+function LoggedIn() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Congrats, you logged in</h1>
+      </header>
+    </div>
+  );
+}
+function HomePage() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {isLoggedIn ? (
-          <h1>Congratulations, you are logged in!</h1>
-        ) : (
-          <>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React with TypeScript
-            </a>
-          </>
-        )}
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React with TypeScript
+        </a>
       </header>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={HomePage()} />
+        <Route path="/loggedIn" element={LoggedIn()} />
+      </Routes>
+    </Router>
   );
 }
 
